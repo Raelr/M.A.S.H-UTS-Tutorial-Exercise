0bbe1eeaac7eb40a7dbda2ca38e09177e2b95e15
Aryeh <aryehzinn@hotmail.com>
2019-03-12 17:20:16 +1100

    Player Movement
    Added the ability for the player to move using WASD or arrow keys

---
+++ Mash-Game-UTS-Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Vector2 velocity;
+
+    [SerializeField]
+    float MoveSpeed;
+
+    Rigidbody2D rigidbody;
+
+    BoxCollider2D collider;
+
+    private void Awake() {
+
+        collider = GetComponent<BoxCollider2D>();
+
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update() {
+
+        ProcessInput();
+        
+    }
+
+    void ProcessInput() {
+
+        velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        MovePlayer();
+    } 
+
+    void MovePlayer() {
+
+        transform.Translate(velocity * MoveSpeed * Time.deltaTime); 
+    }
+}
